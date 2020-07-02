9a397ecb60b89ba67e4e61d4f66e322bea01bf98
issei126 <issei.nakamura@peroli.jp>
2020-07-02 10:53:14 +0900

    simple_model 解答

--- 05_class_definition/simple_model.rb
+++ 05_class_definition/simple_model.rb
@@ -14,4 +14,77 @@
 # 3. 履歴がある場合、すべての操作履歴を放棄し、値も初期状態に戻す `restore!` メソッドを作成する
 
 module SimpleModel
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def attr_accessor(*args)
+      args.each do |arg|
+        # 初期設定用に通常の動作のwriterを設定
+        define_method "old_#{arg.to_s}=" do |writer_arg|
+          instance_variable_set("@#{arg.to_s}", writer_arg)
+        end
+
+        # 履歴を残すカスタマイズしたwriter
+        define_method "#{arg.to_s}=" do |writer_arg|
+          instance_variable_set("@#{arg.to_s}", writer_arg)
+          @changed = true
+          @attr_changed[arg] = true
+        end
+
+        # 通常と同じreader
+        define_method "#{arg.to_s}" do
+          instance_variable_get("@#{arg.to_s}")
+        end
+
+        # 各変数が変更されたかどうかのメソッド
+        define_method "#{arg.to_s}_changed?" do
+          @attr_changed[arg]
+        end
+      end
+    end
+  end
+
+  def initialize(arg_hash)
+    @changed = false
+
+    # 各インスタンス変数に初期値設定
+    arg_hash.each do |k, v|
+      send("old_#{k.to_s}=", v)
+    end
+
+    # 初期値保存
+    @initial_values = {}.tap do |hash|
+      arg_hash.each do |k, v|
+        hash[k] = v
+      end
+    end
+
+    # 各変数の変更状態を保持する変数を初期化
+    @attr_changed = {}.tap do |hash|
+      arg_hash.keys.each do |key|
+        hash[key] = false
+      end
+    end
+  end
+
+  def changed?
+    @changed
+  end
+
+  def restore!
+    # 初期値に戻す
+    @initial_values.each do |key, value|
+      instance_variable_set("@#{key.to_s}", value)
+    end
+
+    # 履歴を戻す
+    @changed = false
+    @attr_changed = {}.tap do |hash|
+      hash.keys.each do |key|
+        hash[key] = false
+      end
+    end
+  end
 end
